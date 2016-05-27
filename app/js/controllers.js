8aa404676667f74c8f6dead72ecd691ca2f65bc6
Richard <richahn2@gmail.com>
2016-05-27 17:01:55 -0400

    Added ability to add Q&A's and display the Q&A list. The list can be filtered and reordered as well.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,6 +1,37 @@
 'use strict' // Helps me write good js
 
 // Load module
-
+var qandaApp = angular.module('qandaApp', []);
 
 // Register controllers
+qandaApp.controller('qandaController', ['$scope', function($scope) {
+  // Set default question list
+  $scope.qas = [
+    {
+      question: 'Question 1',
+      answer: 'Answer 1'
+    },
+    {
+      question: 'Question 2',
+      answer: 'Answer 2'
+    }
+  ];
+  // Set default ordering and qanda prompt
+  $scope.qanda = {
+    ordering: 'question',
+    newQuestion: '',
+    newAnswer: ''
+  };
+
+  $scope.newQanda = function() {
+    // TODO: Check for empty question(show error), check for empty answer(add unanswered tag)
+    $scope.qas.push({
+      question: $scope.qanda.newQuestion,
+      answer: $scope.qanda.newAnswer
+    });
+
+    // Reset qanda prompt
+    $scope.qanda.newQuestion = '';
+    $scope.qanda.newAnswer = '';
+  };
+}]);
